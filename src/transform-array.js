2250a856c21fe9294577770d95bd9824e65d9424
rhjje <numelen@gmail.com>
2020-09-27 23:34:57 +0300

    feat: done hanoi-tower-task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
+module.exports = function transform(/*arr*/) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  // let result=[];
+  //   if (!Array.isArray(arr)) throw new Error();
+
+  //   for(let i=0; i<arr.length;i++){
+  //       if (arr[i]==='--discard-next'){
+  //           i+=1;
+  //       } else if (arr[i]==='--discard-prev') {
+  //               if (result.length) result.pop();
+  //       } else if (arr[i]==='--double-next') {
+  //               if (i<arr.length-1) result.push(arr[i+1]);
+  //       } else if (arr[i]==='--double-prev') {
+  //               if (i) result.push(arr[i-1]);
+  //       } else {
+  //           result.push(arr[i]);
+  //       }
+  //   }
+    
+  //   return result;
 };
+
+
+
+
+
+
