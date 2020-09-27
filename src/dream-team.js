ebc75ba08c8b1053c3831dc98c0bee0026f4dd12
rhjje <numelen@gmail.com>
2020-09-27 17:50:49 +0300

    feat: completed count cats and dream team tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  if (members === []) {
+    return null;
+  }
+
+  let arrFormatted = [];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof(members[i]) === 'string') {
+      arrFormatted.push(members[i].trim());
+    }
+  }
+
+  let result = [];
+
+  for (let i = 0; i < arrFormatted.length; i++) {
+    if (typeof(arrFormatted[i]) === 'string') {
+      result.push(arrFormatted[i][0].toUpperCase());
+    }
+  }
+
+  return result.sort().join('');
 };
+
+
+
+
+
