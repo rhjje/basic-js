f57b685499d55601963661eaace31dc11875857a
rhjje <numelen@gmail.com>
2021-03-13 22:16:31 +0300

    feat: add old solution to count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  return matrix.flat().filter(item => item === '^^').length;
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === '^^') {
+        count++;
+      }
+    }
+  }
+
+  return count;
 };
